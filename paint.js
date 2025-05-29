9ca981c09ce9cff21d372c34936a99554dcf2968
Joe Milius <joemilius@gmail.com>
2025-05-29 06:34:37 -0600

    drawing on canvas working and lined up with cursor

--- paint.js
+++ paint.js
@@ -1,14 +1,26 @@
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
+console.log(ctx)
+canvas.width = 800
+canvas.height = 600
+// ctx.lineWidth = 5
+// ctx.fillStyle = "rgb(255 0 0)"
+let prevMouseX
+let prevMouseY
+
+
 
 
 let isDrawing = false;
 
     canvas.addEventListener('mousedown', (e) => {
+        console.log(e)
         console.log("begin")
         isDrawing = true;
+        prevMouseX = e.offsetX 
+        prevMouseY = e.offsetY 
         ctx.beginPath(); // Start a new path
-        ctx.moveTo(e.offsetX, e.offsetY); // Move to the starting point
+        ctx.moveTo(prevMouseX , prevMouseY ); // Move to the starting point
     });
 
     canvas.addEventListener('mouseup', () => {
@@ -17,11 +29,15 @@ let isDrawing = false;
     });
 
     canvas.addEventListener('mousemove', (e) => {
-        console.log("make line")
+        
         if (isDrawing) {
-            ctx.lineTo(e.offsetX, e.offsetY); // Draw a line to the current mouse position
-            ctx.stroke(); // Stroke the line
+            console.log("make line")
             ctx.beginPath(); // Start a new path
-            ctx.moveTo(e.offsetX, e.offsetY); // Move to the current mouse position
+            ctx.moveTo(prevMouseX , prevMouseY); // Move to the current mouse position
+            ctx.lineTo(e.offsetX , e.offsetY); // Draw a line to the current mouse position
+            ctx.stroke(); // Stroke the line
+            prevMouseX = e.offsetX 
+            prevMouseY = e.offsetY 
+            
         }
     });
